9d3d11660baa0c510e558e0480e38b006af442cf
Masaxx <64898857+Masaxx@users.noreply.github.com>
2025-09-16 16:35:30 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "src/assets"),
+      '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, 'shared'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
   build: {
